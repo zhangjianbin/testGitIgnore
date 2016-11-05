2dafab480b7806f2a39cbe4a32a489e5107356f9
zhangjianbinge <zyb@bdtrip.com.cn>
2016-11-05 13:13:08 +0800

    init

---
+++ gitignoreTest/gitignoreTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitignoreTest
+//
+//  Created by zhangjianbin on 2016/11/5.
+//  Copyright © 2016年 zhangjianbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
