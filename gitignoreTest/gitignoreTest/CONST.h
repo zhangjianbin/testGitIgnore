a0d4f2a9f71a57fd975fbb1a2a59dd378a77b9f7
zhangjianbinge <zyb@bdtrip.com.cn>
2016-11-05 13:15:17 +0800

    add const

---
+++ gitignoreTest/gitignoreTest/CONST.h
@@ -0,0 +1,13 @@
+//
+//  CONST.h
+//  gitignoreTest
+//
+//  Created by zhangjianbin on 2016/11/5.
+//  Copyright © 2016年 zhangjianbin. All rights reserved.
+//
+
+#ifndef CONST_h
+#define CONST_h
+
+
+#endif /* CONST_h */
